1f071a399607df9dafba29ffd2cb105cee068c3b
Vladimir Morozov <vmorozov@microsoft.com>
2020-04-01 15:08:53 -0700

    Added Microsoft.ReactNative project reference and a stub fro the ReactPackageProvider

---
+++ windows/RNFetchBlob/ReactPackageProvider.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "ReactPackageProvider.h"
+#include "ReactPackageProvider.g.cpp"
+
+//#include "SampleModuleCpp.h"
+
+using namespace winrt::Microsoft::ReactNative;
+
+namespace winrt::RNFetchBlob::implementation
+{
+
+void ReactPackageProvider::CreatePackage(IReactPackageBuilder const& /*packageBuilder*/) noexcept
+{
+   //AddAttributedModules(packageBuilder);
+}
+
+} // namespace namespace winrt::RNFetchBlob::implementation
